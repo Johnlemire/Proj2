6b111cb87b99b076138a88818370a42f2fbd6dbf
johnlemire23 <johnsq125@gmail.com>
2015-09-25 01:09:02 -0500

    fri 1

---
+++ Data.c
@@ -0,0 +1,18 @@
+//**********************************/
+// John lemire
+// Data.c : Contains data strucs and data manipulation calls
+// Project 2. Dr Coffee 
+//
+
+//************************************
+// Customer 
+//
+
+struct Customer {
+
+	int priorityNum;
+	int aTime;
+	int dTime;
+	struct Customer * next;
+
+}
